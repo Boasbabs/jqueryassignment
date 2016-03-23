d697d3a12f062eb28a20b25b68f6269a3c658faa
Boasbabs <babalolasimeon@gmail.com>
2016-03-23 20:51:29 +0530

    worked the js solution  in jquery

---
+++ app.js
@@ -0,0 +1,52 @@
+//THE JS SOLUTIONS WAS MODIFIED TO JQUERY.
+
+
+ function q1() {
+ 	$("#para1").text("I am Id of para1");
+ 	 }
+
+function q2() {
+ 	$(".class1").text("I am class1");
+ 	 }
+
+function q3() {
+	console.log($("p"));
+}
+function q4() {
+	// body...
+	$("#block1").css("background-color", "blue");
+}
+
+function questionFive() {
+	$("#block2").css("font-size", "2.5em");
+}
+function mOver() {
+	$("#block3").css("color","red");
+}
+function mOut() {
+	$("#block3").css("color", "black");
+	// body...
+}
+function hide() {
+	$("#block4").css("visibility", "hidden");
+}
+function q8() {
+	$(".box1").css("background-color", "green");
+}
+ function q9() {
+	$(".box2").css("background-color", "blue");
+ }
+ function q11() {
+ 	$("<p>I got generated on the fly...</p>").insertAfter("#para2");
+ }
+
+ function q12() {
+ 	// var parent = document.getElementById("para3");
+ 	// var child = document.getElementById("para4");
+ 	$("#para4").remove();
+ }
+
+ function q14() {
+ 	// $("#box5").fadeIn("slow");
+ 	$("#box5").slideUp(2000).slideDown(2000);
+ }
